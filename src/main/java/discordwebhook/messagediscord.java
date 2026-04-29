bc7a8d375f56065c4760a6207b7975a375f6221c
chokolatchaud <nounou.kevyn78@gmail.com>
2026-04-29 19:09:59 +0200

    bhjthth

--- src/main/java/discordwebhook/messagediscord.java
+++ src/main/java/discordwebhook/messagediscord.java
@@ -9,12 +9,7 @@ public class messagediscord {
 	public static void sendmessage(String content, String salon) {
 	    try {
 	        // Définition des URLs de webhook (⚠ ne pas exposer publiquement)
-	        String webhookUrlStatue = "https://discordapp.com/api/webhooks/1419249454023708683/CMdf_-O90ms0cPBhuIeA68siA9JuXaLffV0Db6-YmkB-RMdKF1_wNPZrfBOSpblSYvIf";
-	        String webhookUrlMSG = "https://discord.com/api/webhooks/1438185045712965702/Gd2MTOhYDlt02QYbaaB74b1HKDxIaavzYt4f87sxKPKPYFltUOlwvGdwDoOArQEyk3c3";
-	        String webhookUrlModeration = "https://discord.com/api/webhooks/1442135562839654540/QkBt4qYbXirEWu8-wbTQPyQIKKbK8UyUsf8AWoFk4hR9W-zr_3oQ2grdaBKvCfKIqtUb";
-	   
-	        // Sélection du webhook selon le salon
-	        String webhookUrl = webhookUrlS
+	        
 	        if (salon.equalsIgnoreCase("message")) {
 	            webhookUrl = webhookUrlMSG;
 	        } else if (salon.equalsIgnoreCase("statut") || salon.equalsIgnoreCase("status")) {
