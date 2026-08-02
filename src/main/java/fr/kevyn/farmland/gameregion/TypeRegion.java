bb55716b92c9119870e6df806591e77abb166917
chokolatchaud <nounou.kevyn78@gmail.com>
2026-08-02 10:41:53 +0200

    -gameregion

---
+++ src/main/java/fr/kevyn/farmland/gameregion/TypeRegion.java
@@ -0,0 +1,17 @@
+package fr.kevyn.farmland.gameregion;
+
+
+public enum TypeRegion {
+	
+	//BuildBattle
+	HUBGAME,
+	BUILDZONEGAME,
+	STARTINGZONEGAME,
+	WAYPOINTZONEGAME,
+	ENDZONEGAME,
+	
+	//Serveur
+	HUBSERVEUR,
+	Boatrace,
+	BoatraceWaypoint,
+}
