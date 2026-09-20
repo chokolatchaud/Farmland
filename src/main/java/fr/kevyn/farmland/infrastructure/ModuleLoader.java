a1895d975dc4450b46f2ca73b8975818ed4d4e43
chokolatchaud <80534860+chokolatchaud@users.noreply.github.com>
2026-09-20 17:02:21 +0200

    refactor: add ModuleLoader

---
+++ src/main/java/fr/kevyn/farmland/infrastructure/ModuleLoader.java
@@ -0,0 +1,35 @@
+package fr.kevyn.farmland.infrastructure;
+
+import org.bukkit.Bukkit;
+
+import fr.kevyn.farmland.FarmlandMain;
+import fr.kevyn.farmland.modules.GameModule;
+import fr.kevyn.farmland.modules.ModerationModule;
+import fr.kevyn.farmland.modules.MetierModule;
+import fr.kevyn.farmland.modules.PlotModule;
+import fr.kevyn.farmland.modules.SaveModule;
+import fr.kevyn.farmland.modules.SecureWorldEditModule;
+import fr.kevyn.farmland.modules.WebApiModule;
+
+/**
+ * Centralise le chargement des modules du plugin.
+ */
+public final class ModuleLoader {
+
+    private static final long LOAD_DELAY_TICKS = 20L;
+
+    private ModuleLoader() {
+    }
+
+    public static void load(FarmlandMain plugin) {
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            new ModerationModule(plugin).register();
+            new PlotModule(plugin).register();
+            new SaveModule(plugin).register();
+            new GameModule(plugin).register();
+            new SecureWorldEditModule(plugin).register();
+            new WebApiModule(plugin).register();
+            new MetierModule(plugin).register();
+        }, LOAD_DELAY_TICKS);
+    }
+}
