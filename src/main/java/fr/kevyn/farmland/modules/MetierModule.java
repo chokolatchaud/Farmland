b7c9fd1f9537de2f74c7a47bef44cf1ccb390b9f
chokolatchaud <80534860+chokolatchaud@users.noreply.github.com>
2026-09-20 18:59:56 +0200

    refactor: structure MetierModule startup

--- src/main/java/fr/kevyn/farmland/modules/MetierModule.java
+++ src/main/java/fr/kevyn/farmland/modules/MetierModule.java
@@ -30,26 +30,41 @@ public final class MetierModule {
     }
 
     public void register() {
+        loadMarket();
+        registerMarketTask();
+        registerJobListeners();
+        registerJobCommands();
+
+        plugin.getLogger().info("[Metier] Module Metier activé");
+        messagediscord.sendmessage("Module Metier bien lancé", "statut");
+    }
+
+    private void loadMarket() {
         Market market = MarketSave.loadMarket(plugin);
         if (market != null) {
             MarketHolder.set(market);
         }
+    }
 
+    private void registerMarketTask() {
         MarketCalcTask.demarrer(plugin);
+    }
 
+    private void registerJobListeners() {
         plugin.getServer().getPluginManager().registerEvents(new EventMineSpawn(), plugin);
         plugin.getServer().getPluginManager().registerEvents(new HarvestFarmEvent(), plugin);
-        plugin.getCommand("houe").setExecutor(new FarmCommands());
-        plugin.getCommand("pioche").setExecutor(new MineCommands());
-        plugin.getCommand("peche").setExecutor(new FishingCommands());
         plugin.getServer().getPluginManager().registerEvents(new EventPeche(), plugin);
         plugin.getServer().getPluginManager().registerEvents(new KillEventAgriculteur(), plugin);
         plugin.getServer().getPluginManager().registerEvents(new KillEventTueur(), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new BlockFertilizeListener(), plugin);
+    }
+
+    private void registerJobCommands() {
+        plugin.getCommand("houe").setExecutor(new FarmCommands());
+        plugin.getCommand("pioche").setExecutor(new MineCommands());
+        plugin.getCommand("peche").setExecutor(new FishingCommands());
         plugin.getCommand("epee").setExecutor(new épeeCommands());
         plugin.getCommand("hache").setExecutor(new HacheCommands());
-        plugin.getServer().getPluginManager().registerEvents(new BlockFertilizeListener(), plugin);
-
-        plugin.getLogger().info("[Metier] Module Metier activé");
-        messagediscord.sendmessage("Module Metier bien lancé", "statut");
+    }
     }
 }
